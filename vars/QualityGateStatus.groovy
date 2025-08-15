6c52df76cb90b981f8ef2b28b4957dd515192d90
ankitayawale <ankitayawale6552@gmail.com>
2025-08-15 14:29:58 +0530

    dockerbuild

--- vars/QualityGateStatus.groovy
+++ vars/QualityGateStatus.groovy
@@ -1,9 +1,18 @@
 
 
-def call(credentialsId){
+//def call(credentialsId){
 
-waitForQualityGate abortPipeline: false, credentialsId: credentialsId
+//waitForQualityGate abortPipeline: false, credentialsId: credentialsId
 
+//}
+
+
+withSonarQubeEnv('jenkins') {
+    sh 'mvn sonar:sonar'
 }
+timeout(time: 1, unit: 'MINUTES') {
+    waitForQualityGate abortPipeline: false
+}
+
 
 
